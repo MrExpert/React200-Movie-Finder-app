a91acf8926ed0f4962cb82ce769470cd8676bc04
Sam Umirdinov <sam.umirdinov@gmail.com>
2019-07-13 03:39:27 -0700

    redux working

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
+/* eslint no-console: "off" */
 const express = require('express');
 const morgan = require('morgan');
+const axios = require('axios');
 
 const app = express();
 
@@ -7,4 +9,33 @@ app.use(morgan('dev'));
 app.use(express.static('dist'));
 app.use(express.static('public'));
 
+const movieCache = {};
+
+app.get('/movies/:str', (req, res) => {
+  const searchStr = req.params.str;
+  if (!(searchStr in movieCache)) {
+    axios
+      .get(`http://www.omdbapi.com/?s=${searchStr}&apikey=8730e0e&type=movie`)
+      .then((moviesResponse) => {
+        if (moviesResponse.data.Response === 'False') {
+          res.status(204).send('no movies found');
+        }
+        const allMovies = moviesResponse.data.Search.map((currMovie) => {
+          return axios.get(`http://www.omdbapi.com/?i=${currMovie.imdbID}&apikey=8730e0e`);
+        });
+        Promise.all(allMovies)
+          .then((movies) => {
+            const newSearchResults = movies.map(m => m.data);
+            movieCache[searchStr] = newSearchResults;
+            res.status(200).send(newSearchResults);
+          })
+          .catch(err => console.log(err.message));
+      })
+      .catch(err => console.log(err));
+  } else {
+    res.status(200).send(movieCache[searchStr]);
+  }
+});
+
 module.exports = app;
+
